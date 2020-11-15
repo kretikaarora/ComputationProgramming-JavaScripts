8170c6447fd1ad1ffa45de81a7793345dbaa695b
kretika arora <kretikaarora@gmail.com>
2020-11-15 22:09:51 +0530

    [Kretika Arora]Welcome to Programming Constructs

---
+++ SelectionPractice1.js
@@ -0,0 +1,31 @@
+///console read write module is requires for taking input from the user
+io= require('console-read-write');
+///we are declaring the function as async 
+///if func is not async then we will have to give all the inputs at the begining together
+///the function wont take the input when it is called
+///Async makes the func wait for input user
+async function MaxAndMin()
+{
+///reading the numbers for user
+console.log("please enter the 1st  three digit number ");
+let firstNo= await io.read(parseInt());
+console.log("please enter the 2nd  three digit number");
+let secondNo= await io.read(parseInt());
+console.log("please enter the 3rd  three digit number");
+let thirdNo=await io.read(parseInt());
+///maximum value
+if(firstNo>=secondNo & firstNo>=thirdNo)
+    console.log("maximum number is :  "+firstNo);
+else if(secondNo>=firstNo & secondNo>=thirdNo)
+    console.log("maximum number is : "+secondNo);
+else
+    console.log("maximum number is :  "+thirdNo);
+///minimum value    
+if(firstNo<=secondNo & firstNo<=thirdNo)
+    console.log("min number is : "+firstNo);
+else if(secondNo<=firstNo & secondNo<=thirdNo)
+    console.log("min number is : "+secondNo);
+else
+    console.log("min number is : "+thirdNo);
+}
+MaxAndMin();
